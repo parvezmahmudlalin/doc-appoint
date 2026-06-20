95f730258d4533c9a3c9a4c33e58288c9c40f9da
Parvez Mahmud Lalin <parvezmahmudlalin@gmail.com>
2026-06-20 20:11:57 +0600

    authentication environment setup

---
+++ src/lib/auth.js
@@ -0,0 +1,22 @@
+import dns from "node:dns";
+dns.setServers(["8.8.8.8","8.8.4.4","1.1.1.1"]);
+
+
+import { betterAuth } from "better-auth";
+import { MongoClient } from "mongodb";
+import { mongodbAdapter } from "better-auth/adapters/mongodb";
+
+
+
+const client = new MongoClient(process.env.MONGODB_URI);
+const db = client.db("doc-appoint");
+
+export const auth = betterAuth({
+  database: mongodbAdapter(db, {
+    client
+  }),
+  emailAndPassword: {
+    enabled: true,
+  },
+  
+});
